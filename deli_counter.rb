ed05dccd3c45f1c70f859152789ff6795274f75a
Daniel James Stephenson <dan_0502@hotmail.co.uk>
2018-08-06 13:10:43 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,21 +1,28 @@
 # Write your code here.
 
-  def take_a_number(katz_deli, name)
-    katz_deli << name
-    puts "Welcome #{name}, you are number #{katz_deli.length} in line."
-  end
+def take_a_number(katz_deli, name)
+  katz_deli << name
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+end
 
-  def now_serving
-    if katz_deli.length == 0
-      puts "There is no one waiting to be served!"
-    else
-      puts "Currently serving #{katz_deli.shift}."
-    end
+def now_serving(katz_deli)
+  if katz_deli.length == 0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{katz_deli.shift}."
   end
+end
 
-  def line
-    if katz_deli == 0
-      puts "The line is currently empty."
-    else
-      puts "The line is currently:"
+def line(katz_deli)
+  if katz_deli.length == 0
+    puts "The line is currently empty."
+  else
+    message="The line is currently:"
+
+    katz_deli.each_with_index do |value, index|
+      message += " #{index.to_i+1}. #{value}"
     end
+
+    puts "#{message}"
+  end
+end
