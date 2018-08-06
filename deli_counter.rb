25074aa2d81c87b3800bf2581da032f9ad2c6584
Daniel James Stephenson <dan_0502@hotmail.co.uk>
2018-08-06 12:45:49 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,8 +2,15 @@
 katz_deli = []
 
 def line
-  if line == 0
+  if 
+    
+else
+  katz_deli == 0
     puts "The line is currently empty."
   end
 
   def take_a_number(katz_deli, name)
+  end
+
+  def now_serving
+  end
